eb0bc78b0c99a8d3af910f446abbc5e961d93074
derwa <audrius.dervinis@gmail.com>
2023-11-21 10:46:49 +0200

    revalidatePath changet to /

--- actions/postData.js
+++ actions/postData.js
@@ -1,23 +1,22 @@
-'use server'
-import { getXataClient } from "@/src/xata"
-import { revalidatePath } from "next/cache"
-const xata = getXataClient()
-
+"use server";
+import { getXataClient } from "@/src/xata";
+import { revalidatePath } from "next/cache";
+const xata = getXataClient();
 
 export const postData = async (formData) => {
-    const diagnoze = formData.get('diagnoze')
-    const operacija = formData.get('operacija')
-    const planas = Boolean(formData.get("planas"))
-    const ekstra = Boolean(formData.get("ekstra"))
-    const protezas = Boolean(formData.get("protezas"))
+  const diagnoze = formData.get("diagnoze");
+  const operacija = formData.get("operacija");
+  const planas = Boolean(formData.get("planas"));
+  const ekstra = Boolean(formData.get("ekstra"));
+  const protezas = Boolean(formData.get("protezas"));
 
-    await xata.db.Operacijos.create({
-        diagnoze,
-        operacija,
-        planas,
-        ekstra,
-        protezas
-    })
+  await xata.db.Operacijos.create({
+    diagnoze,
+    operacija,
+    planas,
+    ekstra,
+    protezas,
+  });
 
-    revalidatePath('/create')
-}
+  revalidatePath("/");
+};
